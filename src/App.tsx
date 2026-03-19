671ba094da0f12ca9966624d8c2373c1cc4d704e
Victor <victor_brandao@outlook.com>
2026-03-19 17:42:22 -0300

    feat - Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Login from './pages/Login'
+import Home from './pages/Home';
 
 function App() {
 
-  return <Login></Login>;
+  // return <Login></Login>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
+  )
 
 }
 
