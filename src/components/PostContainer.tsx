ef0ab4d092b60e5823501478b996a5d829c2f6c8
Victor <victor_brandao@outlook.com>
2026-03-21 17:04:30 -0300

    feat - PostContainer properly centered and sized

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,8 +1,7 @@
 export default function PostContainer() {
     return (
-        <div className="w-full flex flex-col items-center justify-between">
-            {/* <div className="border border-blue w-160 h-fit flex items-center justify-between"> */}
-            <div className="border border-blue w-160 h-fit">
+        <div className="w-full flex flex-1 flex-col items-center justify-between">
+            <div className="border border-blue w-160 h-full">
                 <p>Posts</p>
                 <p>Posts</p>
                 <p>Posts</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import PostContainer from "../components/PostContainer";
 
 export default function Home() {
     return (
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-between h-screen">
             <Navbar />
             <PostContainer />
             <FooterLogo />
