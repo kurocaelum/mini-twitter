7d6d5fda73c51ef2b50cfa117149dbc4d40c74b8
Victor <victor_brandao@outlook.com>
2026-04-02 23:32:21 -0300

    feat - Post creation date formatted

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,12 +7,15 @@ interface PostProps {
 
 export default function Post({ post }: PostProps) {
     const author = post.authorName;
-    // TODO pegar email do usuário e exibir só o nome de usuário (parte antes do @)
     const emailShort = `@${post.authorName.toLowerCase().split(" ")[0]}`;
-    // TODO formatar para dd/mm/yyyy, sem hora
-    const createdAt = post.createdAt;
     const title = post.title;
     const content = post.content;
+    
+    const year = post.createdAt.split("-")[0];
+    const month = post.createdAt.split("-")[1];
+    const day = post.createdAt.split("-")[2].split(" ")[0];
+    const formattedDate = `${day}/${month}/${year}`;
+
 
     return (
         <div className="flex flex-col items-start p-4 gap-3 w-full h-47.5 bg-white dark:bg-blue-midnight3 border border-blue-steel3 dark:border-gray rounded-xl">
@@ -20,7 +23,7 @@ export default function Post({ post }: PostProps) {
                 <p className="text-blue2 dark:text-white text-[16px] font-bold">{author}</p>
                 <p className="text-gray dark:text-gray2 text-[14px]">{emailShort}</p>
                 <p className="text-gray dark:text-gray2 text-[14px]">·</p>
-                <p className="text-gray dark:text-gray2 text-[14px]">{createdAt}</p>
+                <p className="text-gray dark:text-gray2 text-[14px]">{formattedDate}</p>
             </div>
 
             <div className="flex flex-col items-start gap-1 h-21 w-full ">
